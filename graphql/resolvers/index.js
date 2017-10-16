6b43ac46f99e871b59fde004ec22a9bf07a465b3
emily <limeyb7@gmail.com>
2017-10-16 23:05:33 +0100

    simplify get_locations query

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -3,29 +3,25 @@ const Location = require("../types/location");
 const queries = require("../../mongoose/queries");
 
 module.exports = {
-  hello: () => {
-    return "Hello world";
-  },
+  hello: () => "Hello world",
 
-  getLocation: async ({ id }) => {
-    return await queries
+  getLocation: async ({ id }) =>
+    await queries
       .getLocation(id)
       .then(loc => {
         return new Location(loc);
       })
       .catch(err => {
         console.log(err);
-      });
-  },
+      }),
 
-  getLocations: async ({ rating, geolocation }) => {
-    return await queries
+  getLocations: async ({ rating, geolocation }) =>
+    await queries
       .getLocations(rating || 0, geolocation)
       .then(res => {
         return res.map(loc => new Location(loc));
       })
       .catch(err => {
         console.log(err);
-      });
-  }
+      })
 };
--- mongoose/queries/get_locations.js
+++ mongoose/queries/get_locations.js
@@ -2,35 +2,28 @@ const Location = require("../location");
 
 module.exports = (rating, geolocation) => {
   return new Promise(async (resolve, reject) => {
-    if (geolocation) {
-      try {
-        let locations = await Location.aggregate([
-          {
-            $geoNear: {
-              near: {
-                type: "Point",
-                coordinates: geolocation
-              },
-              distanceField: "distance",
-              spherical: true,
-              maxDistance: 20000
+    try {
+      let locations = await Location.aggregate([
+        geolocation
+          ? {
+              $geoNear: {
+                near: {
+                  type: "Point",
+                  coordinates: geolocation
+                },
+                distanceField: "distance",
+                spherical: true,
+                maxDistance: 20000
+              }
             }
-          },
-          {
-            $match: { rating: { $gte: rating } }
-          }
-        ]);
-        resolve(locations);
-      } catch (err) {
-        reject(err);
-      }
-    } else {
-      try {
-        let locations = await Location.find().where("rating").gte(rating);
-        resolve(locations);
-      } catch (err) {
-        reject(err);
-      }
+          : {},
+        {
+          $match: { rating: { $gte: rating } }
+        }
+      ]);
+      resolve(locations);
+    } catch (err) {
+      reject(err);
     }
   });
 };
